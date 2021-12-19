ec55505bea9eeb8a09b46f8f75adbd8d214b82e6
Lucas Brandstaetter <lucas@brandstaetter.tech>
2021-12-19 03:50:35 +0000

    Add solution 2021d01

--- src/day01.rs
+++ src/day01.rs
@@ -4,10 +4,30 @@ pub fn input_generator(input: &str) -> Vec<u32> {
 }
 
 #[aoc(day1, part1)]
-pub fn solve_part1(_input: &[u32]) -> u32 {
+pub fn solve_part1(input: &[u32]) -> u32 {
+  let mut increases: u32 = 0;
+  let mut last: u32 = input[0];
+  for x in 1..input.len() {
+    if input[x] > last {
+      increases = increases + 1;
+    }
+    last = input[x]
+  }
+
+  return increases;
 }
 
 #[aoc(day1, part2)]
-pub fn solve_part2(_input: &[u32]) -> u32 {
-  return 0
+pub fn solve_part2(input: &[u32]) -> u32 {
+  let mut increases: u32 = 0;
+  let mut last: u32 = input[0] + input[1] + input[2];
+  for x in 1..input.len() - 2 {
+    let value = input[x] + input[x + 1] + input[x + 2];
+    if value > last {
+      increases = increases + 1;
+    }
+    last = value
+  }
+
+  return increases;
 }
