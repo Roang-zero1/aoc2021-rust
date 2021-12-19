2b252c3b687dea50b98e76b63fca65d460dd5161
Lucas Brandstaetter <lucas@brandstaetter.tech>
2021-12-19 03:59:11 +0000

    Add day template

---
+++ src/template.rs
@@ -0,0 +1,14 @@
+#[aoc_generator(day1)]
+pub fn input_generator(input: &str) -> Vec<u32> {
+  input.lines().map(|l| l.parse::<u32>().unwrap()).collect()
+}
+
+#[aoc(day1, part1)]
+pub fn solve_part1(input: &[u32]) -> u32 {
+  return 0;
+}
+
+#[aoc(day1, part2)]
+pub fn solve_part2(input: &[u32]) -> u32 {
+  return 0;
+}
